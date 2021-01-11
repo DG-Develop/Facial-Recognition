621c51f2916e9f259707fb6b6085e00854ef9a81
David Gómez <38304163+DG-Develop@users.noreply.github.com>
2021-01-10 18:18:24 -0600

    Almacenamiento de rostros

--- FacialDetectionWithHaarCascades/videoDetection.py
+++ FacialDetectionWithHaarCascades/videoDetection.py
@@ -2,7 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-faceClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceClassif = cv2.CascadeClassifier('..haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
